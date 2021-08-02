e087f49ed9cace2bb848783b27ab5fb540cd3c9f
chandeo <chandeo@gmail.com>
2021-08-02 19:15:02 -0400

    amigos

---
+++ db/migrate/20210802225428_create_usuarios.rb
@@ -0,0 +1,10 @@
+class CreateUsuarios < ActiveRecord::Migration[6.1]
+  def change
+    create_table :usuarios do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
